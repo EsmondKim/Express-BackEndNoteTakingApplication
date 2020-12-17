cfd8eb46a4c2a67833a0d201a08ba39f694c7cb0
EsmondKim <ekelectronicmail@gmail.com>
2020-12-16 22:18:37 -0600

    More work on routes and experimenting with writes to DB.

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const fs = require("fs");
 const util = require("util");
-
 const readfilePromise = util.promisify(fs.readFile);
+const dbData = require("../data/dbData.js"); 
 
 module.exports = function(app) {
     
@@ -10,16 +10,16 @@ app.get("/api/notes", function (req, res) {
     readfilePromise("db/db.json", "utf8").then((data) => {
     res.json(JSON.parse(data)) 
     console.log(JSON.parse(data));
-
     }).catch((error) => {
-        console.log(error); 
+        console.log(error);
+        res.json(true); 
         });
     });
 
     app.post("/api/notes", function (req, res) {
-        let  = req.body
-        console.log();
-        res.json();
+        dbData.push(req.body);        
+        console.log(req.body);
+        res.json(true);
         });
 
     app.delete("/api/notes", function (req, res) {

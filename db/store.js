1caefc9aa1a8e26a9e19257fb8a0937bf0c9db45
EsmondKim <ekelectronicmail@gmail.com>
2020-12-17 20:50:54 -0600

    Added note taking functionality.

--- db/store.js
+++ db/store.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 const apiRoutes = require("../routes/apiroutes.js")
 
-let dbDataArray = fs.readFile(JSON.parse(db.json));
 
+let dbDataArray = fs.readFile("./db.json");
+console.log(dbDataArray);
 
 
 module.exports = dbDataArray;
--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,40 +1,52 @@
 const path = require("path");
 const fs = require("fs");
 const util = require("util");
-const readfilePromise = util.promisify(fs.readFile);
-const dbData = require("../db/store.js"); 
+//const readfilePromise = util.promisify(fs.readFile);
+//const dbData = require("../db/store.js");
 
-module.exports = function(app) {
-    
-app.get("/api/notes", function (req, res) {
-    readfilePromise("db/db.json", "utf8").then((data) => {
-    res.json(JSON.parse(data)) 
-    console.log(JSON.parse(data));
-    }).catch((error) => {
-        console.log(error);
-        res.json(true); 
-        });
+module.exports = function (app) {
+  // app.get("/api/notes", function (req, res) {
+  //     readfilePromise("db/db.json", "utf8").then((data) => {
+  //     res.json(JSON.parse(data))
+  //     console.log(JSON.parse(data));
+  //     }).catch((error) => {
+  //         console.log(error);
+  //         res.json(true);
+  //         });
+  //     });
+
+  app.get("/api/notes", function (req, res) {
+    fs.readFile(__dirname + "/../db/db.json", function (err, data) {
+      if (err) console.log(err);
+      res.send(JSON.parse(data));
     });
+  });
 
-    app.post("/api/notes", function (req, res) {
-        let newNoteData = req.body;
-        console.log(newNoteData);
-        dbData.push(newNoteData);
-        res.json(newNoteData);
-        });
+  app.post("/api/notes", function (req, res) {
+    fs.readFile(__dirname + "/../db/db.json", function (err, data) {
+      if (err) console.log(err);
+      let array = JSON.parse(data);
+      array.push(req.body);
+      for (let i = 0; i < array.length; i++) {
+        array[i].id = i;
+      }
+      fs.writeFile(__dirname + "/../db/db.json", JSON.stringify(array), function (err, data) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(array);
+        }
+      });
+    });
+  });
 
-    app.delete("/api/notes", function (req, res) {
-        let  = req.body
-        console.log();
-        res.json();
-        });
-    
-}
-
-
-
-  
-
+  app.delete("/api/notes/:id", function (req, res) {
+    console.log(req.params.id);
+    let array = JSON.parse(data);
+    let = req.body;
+    console.log();
+    res.json();
+  });
+};
 
 //need a delete function
-
